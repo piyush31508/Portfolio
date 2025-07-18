ed116bfc7d16d0f5e53a47ce322e3cdfe4cd11db
piyush31508 <chawlapiyush811@gmail.com>
2025-07-19 03:11:01 +0530

    base changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : "/Portfolio",
+  base : "/Portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
