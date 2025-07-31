fd8f67d9df75bf6f4ba325e31a0c283516e96a7b
piyush31508 <chawlapiyush811@gmail.com>
2025-07-31 18:45:32 +0530

    CV link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -51,7 +51,7 @@ export const AboutSection = () => {
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 font-medium transition-all duration-300"
               >
-                View CV
+                CV
               </a>
             </div>
           </motion.div>
